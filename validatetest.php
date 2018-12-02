0198d8da7a12b5ac6e0527b1ff6a7d9ddb14f2f6
cit2018 <45308319+cit2018@users.noreply.github.com>
2018-12-02 16:34:44 +1100

    validated pages

--- validatetest.php
+++ validatetest.php
@@ -4,8 +4,8 @@
 <HEAD>
 <title>GAP HARDWARE Validation page</title>
 		<meta charset="UTF-8">
-		<link rel ="stylesheet" type="text/css" href ="style1.css">
-
+		<link rel ="stylesheet" type="text/css" href ="style1.css" >
+		
 </HEAD><!--Head-->
 <body>
 
@@ -37,73 +37,95 @@
 		$_SESSION['username'] = $username;
 		
 		
-		/* echo "<a href=\"./Products.html?username=$username\">Products</a>"; */
 	}
 	else
 	{
 		echo 'The id or password was incorrect';
 		
 	}
-
-	
 ?>
-
 <div class="content"> 
 	<header>
 
-		 <div class ="logo"> 
-		<img id ="logoimg" src=".\Images\GAPHardware.png" alt="AHlogo" style="width:110px"> 
-		 
+		<div class ="logo"> 
+		<img id ="logoimg" src="./Images/GAPHardware.png" alt="AHlogo" style="width:110px"> 
+		 <span  style="font-size:35px;margin-left:150px;"> GAP HARDWARE STORE</span>
 		</div> <!--logo--> 
-		
-		
 		<div class ="menu">
 			
-			<nav>
-					
-					<a href ="./home.html">Home</a >
-					<a href="./AboutUs.html">About Us</a>
-					<a href="./ContactUs.html"> Contact Us</a>
-					<a href="./all-products.html">Products</a> 
-					<a href="./Help.html"> Help</a>
-					<a href="./Login.html" style="float:right;"> Login</a>
-					
-				
+			<nav>	
+				<a href ="./home.html">Home</a >
+				<a href="./AboutUs.html">About Us</a>
+				<a href="./ContactUs.html"> Contact Us</a>
+				<a href="./all-products.html">Products</a> 
+				<a href="./Help.html"> Help</a>
+				<a href="./Logout.html" style="float:right;"> Logout</a>	
 			</nav>
 		</div><!--menu-->
 
 	</header>
-<div class="middle"> <p> </p><div><p> </p><div><p> <div></div></p>  <div> <p> </p></div><p></p><p> </p>  </div></div><p></p>
+<div class="middle">  
+		<p> </p>
+		<div>
+			<p> </p>
+			<div> 
+				<div></div>  
+				<div>
+					<p> </p>
+				</div>
+				<p></p>
+				<p> </p>
+			</div>
+		</div>
+		<p></p>
+
 <!-- middle div-->
 
 <div class="paraclass">
+				<H3 >GAP HARDWARE PRODUCTS ON SALE</H3> 
+				<H2>Please <A HREF="./Images/Product_order_Form.pdf" target="_blank">click</a> here for Product Order Form </H2>
 			
-				<p align="center"> 
-				<a href="./General_Hardware.html">GeneralHardware</a><br/><!-- General hardware tools link  -->
 				<br/><br/>
-				<img src="./Images/StanleyClawHammer.jpg" alt="StanleyClawHammer" style="width:20%" /></a>
-				<img src="./Images/Stanley20pc.jpg"  alt="Stanley20pc" style="width:20%" />
-				<img src="./Images/StanleyBitSet.jpg"  alt="StanleyBitSet" style="width:20%" />
-				<img src="./Images/TrojanWrench.jpg"  alt="TrojanWrench" style="width:20%" />
-				</p>
-				<p align="center">
-				<a href="./Power_Tools.html"align="left">Power Tools</a><br/><!-- Power tools link  -->
-				<br/><br/>
-				<img src="./Images/RyobiImpact.jpg"  alt="RyobiImpact" style="width:20%" />
-				<img src="./Images/RyobiCircular.jpg"  alt="RyobiCircular" width="20%" />
-				<img src="./Images/RyobiJigsaw.png"  alt="RyobiJigsaw" style="width:20%" />
-				<img src="./Images/RyobiBandsaw.jpg"  alt="RyobiBandsaw" style="width:20%" />
-				</p>
-				<p align="center">
-				<a href="./Garden_Tools.html">Garden Tools</a><br/><!-- Garden tools link  -->
-				<br/><br/>
-				<img src="./Images/RyobiBlowerVac.png"  alt="RyobiBlowerVac" style="width:20%" />
-				<img src="./Images/RyobiBrushcutter.jpg"  alt="RyobiBrushcutter"style="width:20%" />
-				<img src="./Images/RyobiPruner.png"  alt="RyobiPruner" style="width:20%" />
-				<img src="./Images/VictaLawnmower.jpg"  alt="VictaLawnmower" style="width:20%" />
-				</p>
+				<div id ="border">
+							
+			
+					<h3>GeneralHardware</h3>
+					<br/><br/>
+					<p>					
+					
+					<span>StanleyClawHammer $48.90</span><img src="./Images/StanleyClawHammer.jpg" alt="StanleyClawHammer"  /> <br/>
+					<span>Stanley20pc $28.99</span><img src="./Images/Stanley20pc.jpg"  alt="Stanley20pc"  /><br/>
+					
+					<span>StanleyBitSet $29.80</span><img src="./Images/StanleyBitSet.jpg"  alt="StanleyBitSet"  /><br/>
+					<span>TrojanWrench $17.99</span><img src="./Images/TrojanWrench.jpg"  alt="TrojanWrench"  /><br/>
+					</p>
+			
+					<h3>Power Tools</h3>
+					<br/><br/>
+					<p>
+					
+					<span>RyobiImpact $153.00</span><img src="./Images/RyobiImpact.jpg"  alt="RyobiImpact" /><br/>
+					<span>RyobiCircular $115.00</span><img src="./Images/RyobiCircular.jpg"  alt="RyobiCircular" /><br/>
+					<span>RyobiJigsaw $89.00</span><img src="./Images/RyobiJigsaw.png"  alt="RyobiJigsaw"  /><br/>
+					<span>RyobiBandsaw $279.00</span><img src="./Images/RyobiBandsaw.jpg"  alt="RyobiBandsaw"  /><br/>
+					</p> 
+					
+					<h3>Garden Tools</h3>
+					<br/><br/>
+					<p>
+					
+					<span> RyobiBlowerVac $44.00</span><img src="./Images/RyobiBlowerVac.png"  alt="RyobiBlowerVac"  /><br/>
+					<span>RyobiBrushcutter $219.00</span><img src="./Images/RyobiBrushcutter.jpg"  alt="RyobiBrushcutter" /><br/>
+					<span>RyobiPruner $173.00</span><img src="./Images/RyobiPruner.png"  alt="RyobiPruner"  /><br/>
+					<span>VictaLawnmower $702.00</span><img src="./Images/VictaLawnmower.jpg"  alt="VictaLawnmower"  /><br/>
+					</p>
+					
+			</div>
+			
+				<a href="./Product-list.html">View Products as a list</a><br/><br/>
+				<a href="./Product-table.html">View Products price Table</a><br/><br/>
+					
 				<a href="#top" target="_self" >Back to top</a>
-			</p>
 			
 </div><!-- paraclass div-->
 		
